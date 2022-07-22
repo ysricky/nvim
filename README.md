582fb2e36741912d19ec6c344cdcd0b2f8be95fa
ysricky <yosuaricky2606@gmail.com>
2022-07-22 13:46:21 +0700

    add options.lua

---
+++ README.md
@@ -0,0 +1,15 @@
+# nvim
+My personal Neovim config files
+
+## Requirements:
+- [Neovim 0.7.2](https://github.com/neovim/neovim/releases/)
+- clipboard (xclip or wl-clipboard)
+
+### Installation
+- Install [Packer](https://github.com/wbthomason/packer.nvim)
+- Run this command:
+```shell
+nvim +PackerSync
+```
+
+*Neovim from scratch / LunarVim
---
+++ lua/user/keymaps.lua
@@ -0,0 +1,35 @@
+-- Shorten function name
+local keymap = vim.keymap.set
+-- Silent keymap option
+local opts = { silent = true }
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+keymap("n", "<leader>e", ":Lex 25<CR>", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
