c966c270d049b99a1338c9d1e3d36fd1fb6eb4bd
ysricky <yosuaricky2606@gmail.com>
2022-07-22 16:28:55 +0700

    add tokyonight colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,7 +1,11 @@
-local colorscheme = "tokyonight"
+local colorscheme = "dracula" -- or "Tokyonight"
+
+-- vim.g.tokyonight_style = "night" -- different style of tokyonight themes ("storm:default", "night", & "day")
+vim.g.dracula_italic_comment = true
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  vim.notify("colorscheme " .. colorscheme .. " not found!") -- friendlier warning message if colorscheme plugins is not installed
   return
 end
+
