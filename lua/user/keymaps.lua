ddf10c07dd0050e42661b83568b950b359e52f72
ysricky <yosuaricky2606@gmail.com>
2022-07-22 15:16:45 +0700

    add packer.nvim to automatically install plugins

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -22,8 +22,6 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
-keymap("n", "<leader>e", ":Lex 25<CR>", opts)
-
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
 keymap("n", "<C-Down>", ":resize +2<CR>", opts)
@@ -33,3 +31,6 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Toggle Netrw
+keymap("n", "<leader>e", ":Lex 25<CR>", opts)
